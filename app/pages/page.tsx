2a1103b888c96aa979c0b25920d3a4fd4378af50
Fatemehex <fatemehex128@gmail.com>
2026-06-15 20:59:43 +0330

    Login

---
+++ app/pages/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import Header from "@/app/_componant/Header"
+import GetFilms from "@/app/GetFilms"
+
+
+export default function Page() {
+
+  return (
+    <div>
+      <Header />
+      <GetFilms/>
+    </div>
+  )
+}
